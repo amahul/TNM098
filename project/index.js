d73b0a871ae0a0c19ba5da75b67bc464ffd2a357
Mathilda <matwe866@student.liu.se>
2023-05-07 14:31:02 +0200

    can now filter on more specific time

--- project/index.js
+++ project/index.js
@@ -63,6 +63,7 @@ function filterData(datacc, datalc, startTime, endTime) {
   else{
     filteredData=[];
   }
+  drawDataPoints()
 console.log(filteredData);
 }
 
--- project/index_heatmap.js
+++ project/index_heatmap.js
@@ -2,19 +2,20 @@ var ccData = []
 var loyaltyData = []
 var gpsData=[];
 var filteredData=[]
+
 var showCC=true;
 var showLC=true;
 choosenlastDate=MIN_DATE;
 choosenfirstDate=MAX_DATE;
 
+drawImage();
 getDataAndProceed();
-
 
 async function getDataAndProceed() {
   try {
     gpsData = await readJson('/gps.json');
-    createSlider();
-    drawImage();
+    createSlidertime();
+    createSliderDay();
 
   } catch (error) {
     // Handle any errors that may occur
@@ -23,22 +24,19 @@ async function getDataAndProceed() {
 }
 
 
-function filterData(startTime, endTime ) {
+function filterData(startTime, endTime) {
   
+  
+  console.log(startTime)
+  console.log(endTime)
+  //console.timeLog(new Date(gpsData[0].Timestamp).getTime())
     filteredData= gpsData.filter(item => {
       const timestamp = parseInt(new Date(item.Timestamp).getTime());
       //console.log(timestamp)
-      return (timestamp >= startTime  && timestamp <= endTime && item.id == 21)     
+      return (timestamp >= startTime.getTime()  && timestamp <= endTime.getTime() && item.id == 20)     
     });
 
-console.log(MIN_LAT)
-console.log(MAX_LAT)
-
-console.log(MIN_LONG)
-console.log(MAX_LONG)
-
-    console.log(MAPX);
-    console.log(MAPY);
+    drawHeatMap()
     
 }
 
